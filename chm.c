64de34211f7cc634e0af39b7e8b13f33498b9645
Chris <czipperz@gmail.com>
2020-01-12 20:50:49 -0500

    Make all statements a single line

--- chm.c
+++ chm.c
@@ -67,24 +67,19 @@ void hash_map_drop(void* _hash_map) {
     free(hash_map->values);
 };
 
-void hash_map_reserve(void* _hash_map, size_t key_size, size_t value_size,
-                      uint64_t extra, uint64_t (*hashf)(const void*)) {
+void hash_map_reserve(void* _hash_map, size_t key_size, size_t value_size, uint64_t extra, uint64_t (*hashf)(const void*)) {
     struct HashMap* hash_map = (struct HashMap*) _hash_map;
     if (hash_map->count + extra > hash_map->cap / 2) {
         struct HashMap new_hash_map;
         new_hash_map.cap = next_power_of_two((hash_map->count + extra) * 2 - 1);
         new_hash_map.count = 0;
-        new_hash_map._flags = (unsigned char*) calloc(
-            (new_hash_map.cap * 2 + CHAR_BIT - 1) / CHAR_BIT, 1);
+        new_hash_map._flags = (unsigned char*) calloc((new_hash_map.cap * 2 + CHAR_BIT - 1) / CHAR_BIT, 1);
         new_hash_map.keys = (char*) malloc(new_hash_map.cap * key_size);
         new_hash_map.values = (char*) malloc(new_hash_map.cap * value_size);
 
         for (uint64_t i = 0; i < hash_map->cap; ++i) {
             if (hash_map_is_present(hash_map, i)) {
-                hash_map_insert(&new_hash_map, key_size, value_size,
-                                &hash_map->keys[i * key_size],
-                                hashf(&hash_map->keys[i * key_size]),
-                                &hash_map->values[i * value_size]);
+                hash_map_insert(&new_hash_map, key_size, value_size, &hash_map->keys[i * key_size], hashf(&hash_map->keys[i * key_size]), &hash_map->values[i * value_size]);
             }
         }
 
@@ -93,9 +88,7 @@ void hash_map_reserve(void* _hash_map, size_t key_size, size_t value_size,
     }
 }
 
-void* hash_map_get(const void* _hash_map, size_t key_size, size_t value_size,
-                   const void* key, uint64_t hash,
-                   int (*comparef)(const void*, const void*)) {
+void* hash_map_get(const void* _hash_map, size_t key_size, size_t value_size, const void* key, uint64_t hash, int (*comparef)(const void*, const void*)) {
     const struct HashMap* hash_map = (const struct HashMap*) _hash_map;
     if (hash_map->cap == 0) {
         return 0;
@@ -103,11 +96,9 @@ void* hash_map_get(const void* _hash_map, size_t key_size, size_t value_size,
 
     uint64_t index = hash & (hash_map->cap - 1);
     for (;; index = ((index + 1) & (hash_map->cap - 1))) {
-        if (hash_map_is_present(hash_map, index)
-            && comparef(&hash_map->keys[index * key_size], key) == 0) {
+        if (hash_map_is_present(hash_map, index) && comparef(&hash_map->keys[index * key_size], key) == 0) {
             return &hash_map->values[index * value_size];
-        } else if (hash_map_is_present(hash_map, index)
-                   || hash_map_is_tombstone(hash_map, index)) {
+        } else if (hash_map_is_present(hash_map, index) || hash_map_is_tombstone(hash_map, index)) {
             continue;
         } else {
             return 0;
@@ -115,13 +106,11 @@ void* hash_map_get(const void* _hash_map, size_t key_size, size_t value_size,
     }
 }
 
-void hash_map_insert(void* _hash_map, size_t key_size, size_t value_size,
-                     const void* key, uint64_t hash, const void* value) {
+void hash_map_insert(void* _hash_map, size_t key_size, size_t value_size, const void* key, uint64_t hash, const void* value) {
     struct HashMap* hash_map = (struct HashMap*) _hash_map;
     uint64_t index = hash & (hash_map->cap - 1);
     for (;; index = ((index + 1) & (hash_map->cap - 1))) {
-        if (!hash_map_is_present(hash_map, index)
-            || hash_map_is_tombstone(hash_map, index)) {
+        if (!hash_map_is_present(hash_map, index) || hash_map_is_tombstone(hash_map, index)) {
             memcpy(&hash_map->keys[index * key_size], key, key_size);
             memcpy(&hash_map->values[index * value_size], value, value_size);
             hash_map_set_present(hash_map, index);
@@ -131,9 +120,7 @@ void hash_map_insert(void* _hash_map, size_t key_size, size_t value_size,
     }
 }
 
-int hash_map_remove(void* _hash_map, size_t key_size, size_t value_size,
-                    const void* key, uint64_t hash, void* value_out,
-                    int (*comparef)(const void*, const void*)) {
+int hash_map_remove(void* _hash_map, size_t key_size, size_t value_size, const void* key, uint64_t hash, void* value_out, int (*comparef)(const void*, const void*)) {
     struct HashMap* hash_map = (struct HashMap*) _hash_map;
     if (hash_map->cap == 0) {
         return 0;
@@ -141,15 +128,12 @@ int hash_map_remove(void* _hash_map, size_t key_size, size_t value_size,
 
     uint64_t index = hash & (hash_map->cap - 1);
     while (1) {
-        if (!hash_map_is_present(hash_map, index)
-            && !hash_map_is_tombstone(hash_map, index)) {
+        if (!hash_map_is_present(hash_map, index) && !hash_map_is_tombstone(hash_map, index)) {
             return 0;
         }
 
-        if (hash_map_is_present(hash_map, index)
-            && comparef(&hash_map->keys[index * key_size], key) == 0) {
-            memcpy(value_out, &hash_map->values[index * value_size],
-                   value_size);
+        if (hash_map_is_present(hash_map, index) && comparef(&hash_map->keys[index * key_size], key) == 0) {
+            memcpy(value_out, &hash_map->values[index * value_size], value_size);
             hash_map_set_tombstone(hash_map, index);
             --hash_map->count;
             return 1;
